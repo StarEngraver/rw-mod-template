5e8ac995d8e2c63729815b85a046a54f797e1ffc
StarEngraver <engraver@noinverse.com>
2024-02-03 10:55:22 +0800

    feat(repo): Added mod template.
    - Added mod template with source code and def files.

---
+++ src/Code/Code/CheckLog_ModStartup.cs
@@ -0,0 +1,14 @@
+﻿using Verse;
+
+namespace RWModTemplate
+{
+    [StaticConstructorOnStartup]
+    public static class CheckLog_ModStartup
+    {
+        static CheckLog_ModStartup()
+        {
+            Log.Message(System.DateTime.Now.ToString("yyyy-MM-dd hh:mm:ss") + " - " + "Mod Template: Load Successfully.");
+        }
+    }
+}
+
